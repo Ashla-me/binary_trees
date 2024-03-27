731d12ffe59af918ec3bc257094f6ea007f7afc9
Ashla-me <mercyashlamesunday@gmail.com>
2024-03-27 03:53:42 -0700

    arranging in a sequential order

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -8,7 +8,7 @@
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
-		return;
+		return (0);
 
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
